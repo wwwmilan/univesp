01fc6d1807d403cc0adfa5112c00504811e415b7
wwwmilan <wsl@DESKTOP-MIFK9B7>
2025-04-23 17:39:27 -0300

    criando campo para o email da DB

--- PI-3/if0_38781370_XXX.sql
+++ PI-3/if0_38781370_XXX.sql
@@ -1,10 +1,11 @@
 CREATE TABLE alunos (
     id INT AUTO_INCREMENT PRIMARY KEY,
     nome VARCHAR(100) NOT NULL,
-    senha_hash VARCHAR(255) NOT NULL,
     ra CHAR(7) NOT NULL UNIQUE,
     serie VARCHAR(2) NOT NULL,
     turma VARCHAR(10) NOT NULL,
     telefone VARCHAR(15) NOT NULL,
+    email VARCHAR(100) NOT NULL,
+    senha_hash VARCHAR(255) NOT NULL,
     data_cadastro TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
