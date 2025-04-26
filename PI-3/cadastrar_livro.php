a4058497b05e1627eedf30c36a346aa7303e1582
wwwmilan <wsl@DESKTOP-MIFK9B7>
2025-04-26 11:09:02 -0300

    testando o php

--- PI-3/cadastrar_livro.php
+++ PI-3/cadastrar_livro.php
@@ -10,12 +10,25 @@ $host = "sql109.infinityfree.com";
 $db = "if0_38781370_univesp";
 $user = "if0_38781370";
 $pass = "Univesp2025";
-$pass = "Univesp2025";
 
 try {
     $pdo = new PDO("mysql:host=$host;dbname=$db;charset=utf8", $user, $pass);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
+    //inicio da importação dos dados
+    $titulo = $_POST['titulo'];
+
+    //inicio da inserção dos dados
+    $stmt = $pdo->prepare("INSERT INTO cadastroLivros (titulo) 
+                           VALUES (:titulo) 
+    
+    
+    $stmt->execute([
+        ':titulo' => $titulo,
+    ]);
+
+
+// aqui finaliza o try
 }
 
 ?>
