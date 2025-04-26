dbf61c8209e88c2bbc175a1d528bf36145fe1a0d
wwwmilan <wsl@DESKTOP-MIFK9B7>
2025-04-26 11:00:30 -0300

    conectar com o db

--- PI-3/cadastrar_livro.php
+++ PI-3/cadastrar_livro.php
@@ -3,3 +3,19 @@
 // inserir na DB 
 // conferir se seu certo 
 // conferir se deu errado 
+
+<?php
+
+$host = "sql109.infinityfree.com";
+$db = "if0_38781370_univesp";
+$user = "if0_38781370";
+$pass = "Univesp2025";
+$pass = "Univesp2025";
+
+try {
+    $pdo = new PDO("mysql:host=$host;dbname=$db;charset=utf8", $user, $pass);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+}
+
+?>
