15222b6d11a97bc7984d5c188f8a5fd95f6cd9c3
wwwmilan <wsl@DESKTOP-MIFK9B7>
2025-04-26 12:47:14 -0300

    testando o login e senha no topo

--- PI-3/cadastrar_livro.php
+++ PI-3/cadastrar_livro.php
@@ -17,15 +17,36 @@ try {
 
     //inicio da importação dos dados
     $titulo = $_POST['titulo'];
+    $subtitulo = $_POST['subtitulo'];
+    $autor = $_POST['autor'];
+    $editora = $_POST['editora'];
+    $ano = $_POST['ano'];
+    $edicao = $_POST['edicao'];
+    $isbn = $_POST['isbn'];
+    $descricao = $_POST['descricao'];
 
-    //inicio da inserção dos dados
-    $stmt = $pdo->prepare("INSERT INTO cadastroLivros (titulo) 
-                           VALUES (:titulo) 
-    
-    
+
+    $stmt = $pdo->prepare("INSERT INTO cadastroLivros (titulo, subtitulo, autor, editora, ano, edicao, isbn, descricao) 
+                           VALUES (:titulo, :subtitulo, :autor, :editora, :ano, :edicao, :isbn, :descricao)");
+
     $stmt->execute([
         ':titulo' => $titulo,
+        ':subtitulo' => $subtitulo,
+        ':autor' => $autor,
+        ':editora' => $editora,
+        ':ano' => $ano,
+        ':edicao' => $edicao,
+        ':isbn' => $isbn,
+        ':descricao' => $descricao
     ]);
+    header("Location: index.html?status=success"); //se tiver sucesso voltar para index?????
+    exit();
+    
+} catch (Exception $e) { //se der erro vamos direcionar para onde???
+    header("Location: cadastro_livros.html?status=error&message=" . urlencode($e->getMessage()));
+    exit();
+
+
 
 
 // aqui finaliza o try
